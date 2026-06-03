3aa9aeddf3166f56aaf21e2256ba9889be024207
כּוּשִׁי <gso.aligrab@gmail.com>
2026-06-03 07:11:04 +0000

    Mein erste Commit

---
+++ 1_Elementare_Syntax/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double alter = 17;
+Console.WriteLine(alter);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hallo, Welt");
